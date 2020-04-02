3e5eff834051b27ebf092e10ee5bd2821c2ae5e0
Moon Ahmed <moona@indeed.com>
2020-04-01 22:48:23 -0500

    add giftlist completed

--- lab4/giftlist.cpp
+++ lab4/giftlist.cpp
@@ -23,21 +23,32 @@ class children
 
     public:
         children(){};
+
         void setName(string name)
         {
             privateName = name;
         };
+        
         void setList(list<string> gifts)
         {   
             privateGifts = gifts;
         };
+        
         string getName(){
             return privateName;
         };
+        
         list<string> getGifts(){
             return privateGifts;
         };
-        void displayList();
+        
+        void displayList(){
+            for (list<string>::iterator it = privateGifts.begin(); it != privateGifts.end(); ++it)
+            {
+                cout << (*it) << "\n";
+            }
+        };
+
         void addGift(string gift)
         {
             privateGifts.push_back(gift);
@@ -45,49 +56,54 @@ class children
         
 };
 
-
-
 int main()
 {
     string child;
     string singleGift;
-    list<children> giftList; //the list of objects
+    bool namesComplete = false; 
+    list<children> giftList; 
+     
+    while(!namesComplete){
+        cout << "Name for nice list: ";
+        getline(cin, child);
 
-    cout << "Name for nice list: ";
-    getline(cin, child);
-
-    while(child!=""){
-        list<string> listOfGifts; 
-        
-        cout << "add gifts for " << child << endl;
-        cout << "gift: ";
-        
-        getline(cin, singleGift);
-        // Create Child and Gift List and Add First Gift
-        children someChild;
-        someChild.setName(child);
-        someChild.setList(listOfGifts);
-        someChild.addGift(singleGift);
-        
-        while(singleGift!=""){
-            cout << "gift: ";
-            getline(cin, singleGift);
-            someChild.addGift(singleGift);
+        if (child==""){
+            namesComplete = true;
         }
 
-        // giftList.push_back(theChild); //adds the item to the list of the kids with objects
+        if(namesComplete == false)
+        {
+            list<string> singleChildList; 
+            cout << "add gifts for " << child << endl;
+            cout << "gift: ";
+            getline(cin, singleGift);
+            // Create Child and Gift List 
+            children someChild;
+            someChild.setName(child);
+            someChild.setList(singleChildList);
+            someChild.addGift(singleGift);
 
-        // cout << "Name for nice list: ";
-        // getline(cin, child);
+            bool giftListComplete = false;
+            while(!giftListComplete){
+                cout << "gift: ";
+                getline(cin, singleGift);
+                someChild.addGift(singleGift);
+                if(singleGift=="")
+                {
+                    giftListComplete = true;
+                }
+            }
+            giftList.push_back(someChild);
+        }
     }
-
-    // cout << "The list contains";
-    // for (list<children>::iterator it = giftList.begin(); it != giftList.end(); ++it)
-    //     {
-    //         children child = *it;
-    //         cout << "\n" << child.getName() << " ";
-    //         child.displayList();
-    //     }
-
+    
+    // Print the List of Gifts Out
+    for (std::list<children>::iterator it = giftList.begin(); it != giftList.end(); ++it)
+    {
+        children iter = *it;
+        cout << iter.getName() << "'s gifts: " << endl;
+        iter.displayList(); 
+    }
+    
     return 0;
 }
